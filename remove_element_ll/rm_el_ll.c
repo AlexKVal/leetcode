25869140814f49c74df6b6567efaa0355611939f
Alexander Shemetovskiy <alexkval@gmail.com>
2020-08-04 13:11:46 +0100

    remove linked list element C boilerplate

---
+++ remove_element_ll/rm_el_ll.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Definition for singly-linked list.
+typedef struct ListNode {
+  int val;
+  struct ListNode* next;
+} ListNode_t;
+
+ListNode_t* newNode(int val){
+  ListNode_t* node = malloc(sizeof(ListNode_t));
+  node->val = val;
+  node->next = NULL;
+  return node;
+}
+
+ListNode_t* buildLinkedList(int nums[], int numsSize){
+  puts("buildLinkedList");
+  ListNode_t* head = NULL;
+
+  for (int i=0; i < numsSize; i++){
+    ListNode_t* newElement = newNode(nums[i]);
+
+    if (head)
+      head->next = newElement;
+    else
+      head = newElement;
+  }
+
+  return head;
+}
+
+void printLinkedList(ListNode_t* head){
+  puts("printLinkedList");
+  while (head){
+    printf("%d", head->val);
+    if (head->next) printf("->");
+    head = head->next;
+  }
+  putchar('\n');
+}
+
+ListNode_t* removeElements(ListNode_t* head, int val){
+  return head;
+}
+
+// clang rm_el_ll.c && ./a.out
+int main(int argc, char const *argv[]){
+  // int nums[] = {1,2,6,3,4,5,6}; int val = 6; // 1->2->3->4->5
+  // int nums[] = {1,1,2,6,3,1,4,5,6}; int val = 1; // 2,6,3,4,5,6
+  int nums[] = {1,2}; int val = 2; // 1
+
+  int numsSize = sizeof(nums)/sizeof(int);
+  ListNode_t* head = buildLinkedList(nums, numsSize);
+
+  // printLinkedList(head);
+
+  ListNode_t* result = removeElements(head, val);
+
+  printLinkedList(result);
+
+  return 0;
+}
