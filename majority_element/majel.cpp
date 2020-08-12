fe739215f8f99c59a7373cf2c5aa93f7e41e9668
Alexander Shemetovskiy <alexkval@gmail.com>
2020-08-12 14:29:15 +0100

    Improve C++ version

--- majority_element/majel.cpp
+++ majority_element/majel.cpp
@@ -5,7 +5,6 @@
 // more than ⌊ n/2 ⌋ times
 int majorityElement(std::vector<int>& nums) {
   int n = nums.size();
-  if (n == 1 || n == 2) return nums[0];
   int half = n / 2;
   // std::cout << "half:" << half << '\n';
   std::unordered_map<int, int> numOccurrences;
@@ -13,22 +12,22 @@ int majorityElement(std::vector<int>& nums) {
     int num = nums[i];
     if (numOccurrences.find(num) != numOccurrences.end()){
       numOccurrences[num] += 1;
-      if (numOccurrences[num] > half) return num;
     } else {
       numOccurrences[num] = 1;
     }
+    if (numOccurrences[num] > half) return num;
     // std::cout << "numOccurrences[" << num << "]:" << numOccurrences[num] << '\n';
   }
 
-  return -1;
+  return nums[0]; // for 2 elements arrays
 }
 
 // c++ -std=c++11 majel.cpp && ./a.out
 int main(int argc, char const *argv[]){
   // std::vector<int> nums{3,2,3}; // 3
-  // std::vector<int> nums{2,2,1,1,1,2,2}; // 2
+  std::vector<int> nums{2,2,1,1,1,2,2}; // 2
   // std::vector<int> nums{2}; // 2
-  std::vector<int> nums{3, 2}; // 3
+  // std::vector<int> nums{3, 2}; // 3
 
   int res = majorityElement(nums);
 
