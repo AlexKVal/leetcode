8451434b1e163b6054bdcdc720996e1a5b0dd8ec
Alexander Shemetovskiy <alexkval@gmail.com>
2020-08-18 00:36:24 +0100

    Improve Two Sum Python

--- two_sum/two_sum.py
+++ two_sum/two_sum.py
@@ -4,19 +4,13 @@ def twoSum(nums, target):
     :type target: int
     :rtype: List[int]
     """
-    complement_to_index = {}
+    numToIndex = {}
     for index, num in enumerate(nums):
       complement = target - num
-      if not(complement in complement_to_index):
-        complement_to_index[complement] = index
-
-      if num in complement_to_index:
-        other_index = complement_to_index[num]
-        if other_index != index:
-          return [other_index, index]
-
+      if complement in numToIndex:
+        return [numToIndex[complement], index]
+      numToIndex[num] = index
     return []
-    # print(complement_to_index)
 
 print(twoSum([2,5,5,11], 10)) # [ 1, 2 ]
 print(twoSum([3,2,4], 6))     # [ 1, 2 ]
