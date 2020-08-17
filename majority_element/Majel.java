9a023bce15f58000eeedbf8f7feefe4730646ee8
Alexander Shemetovskiy <alexkval@gmail.com>
2020-08-18 00:52:01 +0100

    Majority Element Java - Boyer-Moore

--- majority_element/Majel.java
+++ majority_element/Majel.java
@@ -4,22 +4,29 @@ import java.util.Arrays;
 
 class Majel {
   public static int majorityElement(int[] nums) {
-    int half = nums.length / 2;
-    Map<Integer, Integer> numOccurrences = new HashMap<Integer, Integer>();
+    // int half = nums.length / 2;
+    // Map<Integer, Integer> numOccurrences = new HashMap<Integer, Integer>();
 
-    for (int num: nums){
-      numOccurrences.compute(num, (k, v) -> (v == null) ? 1 : v + 1);
-      if (numOccurrences.get(num) > half) return num;
+    // for (int num: nums){
+    //   numOccurrences.compute(num, (k, v) -> (v == null) ? 1 : v + 1);
+    //   if (numOccurrences.get(num) > half) return num;
+    // }
+
+    // Boyer-Moore
+    int votes = 0;
+    Integer candidate = null;
+    for(int num : nums){
+      if (votes == 0) candidate = num;
+      votes += (num == candidate) ? +1 : -1;
     }
-
-    return nums[0];
+    return candidate;
   }
 
   public static void main(String[] args){
-    int[] nums = {3,2,3}; // 3
+    // int[] nums = {3,2,3}; // 3
     // int[] nums = {3,2}; // 3
     // int[] nums = {3}; // 3
-    // int[] nums = {-1,-1,2147483647}; // -1
+    int[] nums = {-1,-1,2147483647}; // -1
 
     int res = majorityElement(nums);
 
