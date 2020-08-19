72b8873d40fac2826d5a325e87e258de2ec975be
Alexander Shemetovskiy <alexkval@gmail.com>
2020-08-19 14:54:24 +0100

    Design HashMap C - open addressing - finished

--- design_hashmap/hashmap.c
+++ design_hashmap/hashmap.c
@@ -3,7 +3,6 @@
 #include <stdbool.h>
 
 #define initialCapacity 8
-
 #define NONTAKEN -1
 
 typedef struct {
@@ -40,7 +39,6 @@ int hash(int key, int size){
   return abs(key) % size;
 }
 
-// TODO set needResizing when shift is > 3
 // true - success, false - not set
 bool setValue(MyHashMap* obj, int key, int value){
   int entryIndex = hash(key, obj->maxEntries);
@@ -119,7 +117,6 @@ void myHashMapPut(MyHashMap* obj, int key, int value) {
 
 // Returns the value to which the specified key is mapped,
 // or -1 if this map contains no mapping for the key
-#define NOMAPPING -1
 int myHashMapGet(MyHashMap* obj, int key) {
   int entryIndex = hash(key, obj->maxEntries);
   int shouldBeIndex = entryIndex;
@@ -128,7 +125,7 @@ int myHashMapGet(MyHashMap* obj, int key) {
   while (entryIndex < obj->maxEntries){
     MapEntry* entry = &obj->entries[entryIndex];
 
-    if (entry->key == NONTAKEN) return NOMAPPING;
+    if (entry->key == NONTAKEN) return NONTAKEN;
     if (entry->key == key){
       if (shouldBeIndex != entryIndex)
         printf(" getting %d probed +%d\n", key, entryIndex - shouldBeIndex);
@@ -138,12 +135,48 @@ int myHashMapGet(MyHashMap* obj, int key) {
     entryIndex++;
   }
 
-  return NOMAPPING;
+  return NONTAKEN;
 }
 
 /** Removes the mapping of the specified value key if this map contains a mapping for the key */
 void myHashMapRemove(MyHashMap* obj, int key) {
-  // TODO
+  int entryIndex = hash(key, obj->maxEntries);
+  int shouldBeIndex = entryIndex;
+
+  printf("freeing %d\n", key);
+  while (entryIndex < obj->maxEntries){
+    MapEntry* entry = &obj->entries[entryIndex];
+
+    if (entry->key == NONTAKEN) return; // noop
+
+    if (entry->key == key){ // free it
+      entry->key = NONTAKEN;
+      if (shouldBeIndex != entryIndex)
+        printf(" found at +%d\n", entryIndex - shouldBeIndex);
+      break;
+    }
+
+    entryIndex++;
+  }
+
+  // check for the next keys if they were shifted and unshift them
+  entryIndex++;
+  while (entryIndex < obj->maxEntries){
+    MapEntry* entry = &obj->entries[entryIndex];
+
+    if (entry->key == NONTAKEN) break;
+
+    int shouldBeIndex = hash(entry->key, obj->maxEntries);
+    if (entryIndex != shouldBeIndex){
+      printf(" unshifting %d %d<<%d\n", entry->key, entryIndex - 1, entryIndex);
+      MapEntry* prevEntry = &obj->entries[entryIndex - 1];
+      prevEntry->key = entry->key;
+      prevEntry->value = entry->value;
+      entry->key = NONTAKEN;
+    }
+
+    entryIndex++;
+  }
 }
 
 void myHashMapFree(MyHashMap* obj) {
@@ -166,10 +199,25 @@ int main(int argc, char const *argv[]){
   MyHashMap* map = myHashMapCreate();
 
   puts("== writing ==");
-  // myHashMapPut(map, 1, 11);
-  // myHashMapPut(map, 2, 22);
-  // myHashMapPut(map, 2, 222); // update
-  // myHashMapPut(map, 17, 17);
+  myHashMapPut(map, 1, 11);
+  myHashMapPut(map, 2, 22);
+  myHashMapPut(map, 2, 222); // update
+  myHashMapPut(map, 17, 17);
+  myHashMapPut(map, 18, 18);
+  myHashMapPut(map, 19, 19);
+  // puts("== reading ==");
+  // printf("1: %d\n", myHashMapGet(map, 1));
+  // printf("2: %d\n", myHashMapGet(map, 2));
+  // printf("17: %d\n", myHashMapGet(map, 17));
+  // printf("18: %d\n", myHashMapGet(map, 18));
+  // printf("19: %d\n", myHashMapGet(map, 19));
+  printHashMap(map);
+  puts("== deleting ==");
+  myHashMapRemove(map, 17);
+  myHashMapRemove(map, 18);
+  myHashMapRemove(map, 19);
+  printHashMap(map);
+
   // myHashMapPut(map, 3, 33);
   // myHashMapPut(map, 4, 44);
   // myHashMapPut(map, 8, 88);
@@ -181,15 +229,10 @@ int main(int argc, char const *argv[]){
   // printHashMap(map);
 
   // collision in rehash - hard to get
-  myHashMapPut(map, 22, 22);
-  myHashMapPut(map, 14, 14);
-  myHashMapPut(map, 15, 15);
-  printHashMap(map);
-
-  // puts("== reading ==");
-  // printf("1: %d\n", myHashMapGet(map, 1));
-  // printf("2: %d\n", myHashMapGet(map, 2));
-  // printf("17: %d\n", myHashMapGet(map, 17));
+  // myHashMapPut(map, 22, 22);
+  // myHashMapPut(map, 14, 14);
+  // myHashMapPut(map, 15, 15);
+  // printHashMap(map);
 
   myHashMapFree(map);
 
