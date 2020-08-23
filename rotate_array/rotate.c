39ace87f4912ca5cb016aa42763df3dd47d384de
Alexander Shemetovskiy <alexkval@gmail.com>
2020-08-23 22:40:32 +0100

    Rotate Array C with aux array

--- rotate_array/rotate.c
+++ rotate_array/rotate.c
@@ -1,20 +1,5 @@
 #include <stdio.h>
-
-// it's guaranteed that nums[i] fits in a 32 bit-signed integer
-// rotate right >>
-// brute force O(n^2)
-void rotate(int* nums, int numsSize, int k){
-  for(int r = 0; r < k; r++){
-    // printf("r:%d\n", r);
-    int lastNum = nums[numsSize - 1];
-    // printf("last:%d\n", lastNum);
-    for(int i = (numsSize - 1); i != 0; i--){
-      // printf("%d:%d %d=>%d\n", i, i-1, nums[i], nums[i - 1]);
-      nums[i] = nums[i - 1];
-    }
-    nums[0] = lastNum;
-  }
-}
+#include <stdlib.h>
 
 void printArray(int* arr, int size){
   printf("[ ");
@@ -24,9 +9,33 @@ void printArray(int* arr, int size){
   printf("]\n");
 }
 
+// it's guaranteed that nums[i] fits in a 32 bit-signed integer
+// rotate right >>
+void rotate(int* nums, int numsSize, int k){
+  if (k > numsSize) k = k % numsSize; // handle over-rotations
+
+  int overflowStart = numsSize-k;
+  if (overflowStart <= 0) return;
+
+  int* overflow = calloc(k, sizeof(int));
+
+  for(int oIdx = 0, i = overflowStart; i < numsSize; i++)
+    overflow[oIdx++] = nums[i];
+
+  for(int i = overflowStart-1; i >= 0; i--)
+    nums[i+k] = nums[i];
+
+  for(int i = 0; i < k; i++)
+    nums[i] = overflow[i];
+
+  free(overflow);
+}
+
 // clang rotate.c && ./a.out
 int main(int argc, char const *argv[]){
-  int nums[] = {1,2,3,4,5,6,7}; int k = 3; // [5,6,7,1,2,3,4]
+  // int nums[] = {1,2,3,4,5,6,7}; int k = 3; // [5,6,7,1,2,3,4]
+  int nums[] = {1,2}; int k = 3; // [2,1]
+  // int nums[] = {1,2}; int k = 1; // [2,1]
   int numsSize = sizeof(nums)/sizeof(int);
 
   rotate(nums, numsSize, k);
@@ -34,3 +43,15 @@ int main(int argc, char const *argv[]){
   printArray(nums, numsSize);
   return 0;
 }
+
+// brute force
+// for(int r = 0; r < k; r++){
+//   // printf("r:%d\n", r);
+//   int lastNum = nums[numsSize - 1];
+//   // printf("last:%d\n", lastNum);
+//   for(int i = (numsSize - 1); i != 0; i--){
+//     // printf("%d:%d %d=>%d\n", i, i-1, nums[i], nums[i - 1]);
+//     nums[i] = nums[i - 1];
+//   }
+//   nums[0] = lastNum;
+// }
