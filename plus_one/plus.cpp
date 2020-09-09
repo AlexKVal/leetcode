049c46c60d0319b6e34097ad9a61f212ab805a20
Alexander Shemetovskiy <alexkval@gmail.com>
2020-09-09 12:42:12 +0100

    Plus One - C++ using vector#insert

--- plus_one/plus.cpp
+++ plus_one/plus.cpp
@@ -1,15 +1,9 @@
 #include <iostream>
 #include <vector>
 
-// #include <algorithm> // copy
-// #include <iterator> // back_inserter
-// std::copy(digits.begin(), digits.end(), std::back_inserter(resultArr));
-
 std::vector<int> plusOne(std::vector<int>& digits) {
-	int newSize = digits.size();
-
 	bool carryOver = false;
-	for(int i=newSize-1; i>=0; i--){
+	for(int i=digits.size()-1; i>=0; i--){
 		if(digits[i] == 9){
 			carryOver = true;
 			digits[i] = 0;
@@ -19,18 +13,8 @@ std::vector<int> plusOne(std::vector<int>& digits) {
 			break;
 		}
 	}
-
-	if (carryOver) newSize++;
-	std::vector<int> resultArr(newSize);
-
-	int i = carryOver ? 1 : 0;
-	if (carryOver) resultArr[0] = 1;
-	for(auto iDgt = digits.begin(); iDgt != digits.end(); iDgt++){
-		resultArr[i] = *iDgt;
-		i++;
-	}
-
-	return resultArr;
+	if (carryOver) digits.insert(digits.begin(), 1);
+	return digits;
 }
 
 void printArray(std::vector<int> arr){
@@ -42,8 +26,8 @@ void printArray(std::vector<int> arr){
 
 // c++ -std=c++11 plus.cpp && ./a.out
 int main(int argc, char const *argv[]){
-	std::vector<int> digits {4,3,2,9}; // [4,3,3,0]
-	// std::vector<int> digits {9,9,9,9}; // [1,0,0,0,0]
+	// std::vector<int> digits {4,3,2,9}; // [4,3,3,0]
+	std::vector<int> digits {9,9,9,9}; // [1,0,0,0,0]
 
 	auto res = plusOne(digits);
 
@@ -51,3 +35,32 @@ int main(int argc, char const *argv[]){
 
 	return 0;
 }
+
+// with new array
+// std::vector<int> plusOne(std::vector<int>& digits) {
+// 	int newSize = digits.size();
+
+// 	bool carryOver = false;
+// 	for(int i=newSize-1; i>=0; i--){
+// 		if(digits[i] == 9){
+// 			carryOver = true;
+// 			digits[i] = 0;
+// 		} else {
+// 			carryOver = false;
+// 			digits[i]++;
+// 			break;
+// 		}
+// 	}
+
+// 	if (carryOver) newSize++;
+// 	std::vector<int> resultArr(newSize);
+
+// 	int i = carryOver ? 1 : 0;
+// 	if (carryOver) resultArr[0] = 1;
+// 	for(auto iDgt = digits.begin(); iDgt != digits.end(); iDgt++){
+// 		resultArr[i] = *iDgt;
+// 		i++;
+// 	}
+
+// 	return resultArr;
+// }
